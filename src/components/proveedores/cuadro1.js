ea18b91d86ffa8cbe9355cc933a3b4df79f357ae
AndreaF5rnandez <andreafernandez1257@gmail.com>
2023-05-18 23:08:11 -0300

    Vambios

--- src/components/proveedores/cuadro1.js
+++ src/components/proveedores/cuadro1.js
@@ -10,7 +10,6 @@ export function Cuadro1(props) {
         className="rounded-3 py-5 shadow"
         style={{
           width: '15%',
-          height: "50%",
           backgroundColor: "white",
           padding: "20px",
           margin: "0px 10px 0px 10px"
